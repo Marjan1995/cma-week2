480f4d238cf913f628827dbacf786f224f662f2f
Marjan Tabataba <marjan.tabatabavakili@uzh.ch>
2021-05-05 17:33:18 +0200

    finish until task 3

--- Exercise2.R
+++ Exercise2.R
@@ -14,40 +14,28 @@ wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, rem
 wildschwein_BE <- group_by(wildschwein_BE,TierID)
 wildschwein_BE$timelag  <- as.integer(difftime(lead(wildschwein_BE$DatetimeUTC), wildschwein_BE$DatetimeUTC, units = "sec"))
 wildschwein_BE$timelag
-str(wildschwein_BE$timelag)
-numbers <- 1:10
-numbers
-
-library(dplyr)
-lead(numbers)
-lead(numbers,n = 2)
-lag(numbers)
-lag(numbers,n = 5)
-lag(numbers,n = 5, default = 0)
-summary(wildschwein_BE$timelag)
-wildschwein_BE <- mutate(wildschwein_BE,timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC)))
-summarise(wildschwein_BE, mean = mean(timelag, na.rm = T))
-library(ggplot2)
-ggplot(wildschwein_BE, 
-       aes(x=wildschwein_BE$DatetimeUTC, y= wildschwein_BE$timelag, color=wildschwein_BE$TierID)) +
-  geom_line(size=1) +
-  labs(title = "Tracking")
+wildschwein_BE <- mutate(wildschwein_BE,timelag = as.numeric(difftime(lead(DatetimeUTC),DatetimeUTC)))
+summarise(wildschwein_BE, min_time = min(DatetimeUTC),
+          max_time = max(DatetimeUTC),
+          Tracking_Period = difftime(max_time,min_time),
+          min_lag = min(timelag, na.rm = T),
+          max_lag = max(timelag, na.rm = T),
+          median_lag = median(timelag, na.rm = T))
+ggplot(wildschwein_BE)+geom_point(aes(x= wildschwein_BE$DatetimeUTC, y= wildschwein_BE$timelag, color=wildschwein_BE$TierID))
   
 # Task 1
-# How many individuals were tracked? There are 3 individuals, who were tracked. (TierID are only three: 002A,016A and 018A)
-# For how long were the individual tracked? Are there gaps? 002A has been tracked for 1286 sec, 016A for 1412 sec and 018A for 1599 sec
-# Were all individuals tracked concurrently or sequentially? 002A was tracked seperately, but 016A and 018A werde tracked concurrently
-# What is the temporal sampling interval between the locations? From October 2014 until July 2015
+# There are 3 individuals, who were tracked. (TierID are only three: 002A,016A and 018A)
+# 002A has been tracked for 338.5834 days, 016A for 234.6663 days and 018A for 261.6559 days.
+# They were tracked concurrently.
+# The temporal sampling interval is various.
 
 
 # Task 2
-wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",")
-wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
 wildschwein_BE <- group_by(wildschwein_BE,TierID)
-wildschwein_BE <- mutate(steplength = as.numeric(sqrt((wildschwein_BE$E - lead(wildschwein_BE$E,1))^2 + (wildschwein_BE$N - lead(wildschwein_BE$N,1))^2)))
+wildschwein_BE$steplength <- as.numeric(sqrt((wildschwein_BE$E - lead(wildschwein_BE$E,1))^2 + (wildschwein_BE$N - lead(wildschwein_BE$N,1))^2))
+wildschwein_BE$speed <- as.numeric(wildschwein_BE$steplength/wildschwein_BE$timelag)
 
-
-# The speed unit is m per second. 
+# The speed unit is m per second. Because the unit of geometry is meter.
 
 # Task 3
 caro60 <- read_delim("caro60.csv",",")
@@ -62,116 +50,84 @@ nrow(caro60)
 nrow(caro_3)
 nrow(caro_6)
 nrow(caro_9)
+caro60 <- group_by(caro60,TierID)
 caro60$timelag  <- as.integer(difftime(lead(caro60$DatetimeUTC), caro60$DatetimeUTC, units = "sec"))
 caro60$timelag
-summarise(caro60, mean = mean(timelag, na.rm = T))
-str(caro60$timelag)
-numbers <- 1:10
-numbers
-library(dplyr)
-lead(numbers)
-lead(numbers,n = 2)
-lag(numbers)
-lag(numbers,n = 5)
-lag(numbers,n = 5, default = 0)
-summary(caro60$timelag)
-caro60 <- mutate(caro60,timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC)))
-summarise(caro60, mean = mean(timelag, na.rm = T))
-aa <- c(caro60$E)
-aa
-bb <- c(caro60$N)
-bb
-euclidean <- function(aa,bb) sqrt(sum(aa-bb)^2)
-euclidean(aa,bb)
-speedlenghtcaro60 <- euclidean(aa,bb)
-speedcaro60 <- speedlenghtcaro60/60
-speedcaro60
-#For the dataset caro60 is the timelag 60s.The speedlenght is 273095159m. And the speed 4551586m/s.
-
+caro60$steplength <- as.numeric(sqrt((caro60$E - lead(caro60$E,1))^2 + (caro60$N - lead(caro60$N,1))^2))
+caro60$speed <- as.numeric(caro60$steplength/caro60$timelag)
+caro_3 <- group_by(caro_3,TierID)
 caro_3$timelag  <- as.integer(difftime(lead(caro_3$DatetimeUTC), caro_3$DatetimeUTC, units = "sec"))
 caro_3$timelag
-str(caro_3$timelag)
-summarise(caro_3, mean = mean(timelag, na.rm = T))
-numbers <- 1:10
-numbers
-library(dplyr)
-lead(numbers)
-lead(numbers,n = 2)
-lag(numbers)
-lag(numbers,n = 5)
-lag(numbers,n = 5, default = 0)
-summary(caro_3$timelag)
-caro_3 <- mutate(caro_3,timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC)))
-summarise(caro_3, mean = mean(timelag, na.rm = T))
-aaa <- c(caro_3$E)
-aaa
-bbb <- c(caro_3$N)
-bbb
-euclidean <- function(aaa,bbb) sqrt(sum(aaa-bbb)^2)
-euclidean(aaa,bbb)
-speedlenghtcaro_3 <- euclidean(aaa,bbb)
-speedcaro_3 <- speedlenghtcaro_3/180
-speedcaro_3
-# For the dataset caro_3 there are this results:180 seconds,91486858 m and 508260.3 m/s.
-
+caro_3$steplength <- as.numeric(sqrt((caro_3$E - lead(caro_3$E,1))^2 + (caro_3$N - lead(caro_3$N,1))^2))
+caro_3$speed <- as.numeric(caro_3$steplength/caro_3$timelag)
+caro_6 <- group_by(caro_6,TierID)
 caro_6$timelag  <- as.integer(difftime(lead(caro_6$DatetimeUTC), caro_6$DatetimeUTC, units = "sec"))
 caro_6$timelag
-str(caro_6$timelag)
-summarise(caro_6, mean = mean(timelag, na.rm = T))
-numbers <- 1:10
-numbers
-library(dplyr)
-lead(numbers)
-lead(numbers,n = 2)
-lag(numbers)
-lag(numbers,n = 5)
-lag(numbers,n = 5, default = 0)
-summary(caro_6$timelag)
-caro_6 <- mutate(caro_6,timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC)))
-summarise(caro_6, mean = mean(timelag, na.rm = T))
-ee <- c(caro_6$E)
-ee
-ff <- c(caro_6$N)
-ff
-euclidean <- function(ee,ff) sqrt(sum(ee-ff)^2)
-euclidean(ee,ff)
-speedlenghtcaro_6 <- euclidean(ee,ff)
-speedcaro_6 <- speedlenghtcaro_6/360
-speedcaro_6
-# For caro_6: 360s, 46426223 m and 128961 m/s.
-
+caro_6$steplength <- as.numeric(sqrt((caro_6$E - lead(caro_6$E,1))^2 + (caro_6$N - lead(caro_6$N,1))^2))
+caro_6$speed <- as.numeric(caro_6$steplength/caro_6$timelag)
+caro_9 <- group_by(caro_9,TierID)
 caro_9$timelag  <- as.integer(difftime(lead(caro_9$DatetimeUTC), caro_9$DatetimeUTC, units = "sec"))
 caro_9$timelag
-str(caro_9$timelag)
-summarise(caro_9, mean = mean(timelag, na.rm = T))
-numbers <- 1:10
-numbers
-library(dplyr)
-lead(numbers)
-lead(numbers,n = 2)
-lag(numbers)
-lag(numbers,n = 5)
-lag(numbers,n = 5, default = 0)
-summary(caro_9$timelag)
-caro_9 <- mutate(caro_9,timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC)))
-summarise(caro_9, mean = mean(timelag, na.rm = T))
-eee <- c(caro_9$E)
-eee
-fff <- c(caro_9$N)
-fff
-euclidean <- function(eee,fff) sqrt(sum(eee-fff)^2)
-euclidean(eee,fff)
-speedlenghtcaro_9 <- euclidean(eee,fff)
-speedcaro_9 <- speedlenghtcaro_9/540
-speedcaro_9
-#For caro_9: 540s, 31405957 m and 58159.18 m/s.
+caro_9$steplength <- as.numeric(sqrt((caro_9$E - lead(caro_9$E,1))^2 + (caro_9$N - lead(caro_9$N,1))^2))
+caro_9$speed <- as.numeric(caro_9$steplength/caro_9$timelag)
 
-# Compare speeds visually in a line plot and also visualize the trajectories in a map.
-# What do the different lines for the different temporal granularities tell you?
+caro60_l <- caro60 %>%
+group_by() %>%
+summarise(do_union = FALSE) %>%
+st_cast("LINESTRING")
+
+caro_3_l <- caro_3 %>%
+group_by() %>%
+summarise(do_union = FALSE) %>%
+st_cast("LINESTRING")
+
+caro_6_l <- caro_6 %>%
+group_by() %>%
+summarise(do_union = FALSE) %>%
+st_cast("LINESTRING")
+
+caro_9_l <- caro_9 %>%
+group_by() %>%
+summarise(do_union = FALSE) %>%
+st_cast("LINESTRING")
 
 
 
+ggplot() + ggtitle("Caro60 bs Caro_3") + xlab("E") + ylab("N") + theme_set(theme_minimal()) + theme(aspect.ratio = 1) +
+  geom_sf(data = caro60, aes(color = "1 Min"), alpha = 0.8) +
+  geom_sf(data = caro60, aes(color = "1 Min"), alpha = 0.8) +
+  geom_sf(data = caro_3, aes(color = "3 Min"), alpha = 0.8) +
+  geom_sf(data = caro_3, aes(color = "3 Min"), alpha = 0.8) +
+  coord_sf(datum = 2056)+
+  scale_color_manual(name = "Interval", breaks = c("1 Min", "3 Min"), values = c("1 Min" = "cyan", "3 Min"= "magenta"))
 
+ggplot() + ggtitle("Caro60 bs Caro_6") + xlab("E") + ylab("N") + theme_set(theme_minimal()) + theme(aspect.ratio = 1) +
+  geom_sf(data = caro60, aes(color = "1 Min"), alpha = 0.8) +
+  geom_sf(data = caro60_l, aes(color = "1 Min"), alpha = 0.8) +
+  geom_sf(data = caro_6, aes(color = "6 Min"), alpha = 0.8) +
+  geom_sf(data = caro_6_l, aes(color = "6 Min"), alpha = 0.8) +
+  coord_sf(datum = 2056)+
+  scale_color_manual(name = "Interval", breaks = c("1 Min", "6 Min"), values = c("1 Min" = "cyan", "6 Min"= "magenta"))
+
+ggplot() + ggtitle("Caro60 bs Caro_9") + xlab("E") + ylab("N") + theme_set(theme_minimal()) + theme(aspect.ratio = 1) +
+  geom_sf(data = caro60, aes(color = "1 Min"), alpha = 0.8) +
+  geom_sf(data = caro60_l, aes(color = "1 Min"), alpha = 0.8) +
+  geom_sf(data = caro_9, aes(color = "9 Min"), alpha = 0.8) +
+  geom_sf(data = caro_9_l, aes(color = "9 Min"), alpha = 0.8) +
+  coord_sf(datum = 2056)+
+  scale_color_manual(name = "Interval", breaks = c("1 Min", "9 Min"), values = c("1 Min" = "cyan", "9 Min"= "magenta"))
+
+ggplot()+
+  ggtitle("Speed with different Interval")+
+  ylab("Speed [m/s]")+
+  xlab("Time [s]")+
+  theme_set(theme_minimal()) +
+  geom_line(data = caro60, aes(x = DatetimeUTC, y = speed, color ="1 Min"))+
+  geom_line(data = caro_3, aes(x = DatetimeUTC, y = speed, color ="3 Min"))+
+  geom_line(data = caro_6, aes(x = DatetimeUTC, y = speed, color ="6 Min"))+
+  geom_line(data = caro_9, aes(x = DatetimeUTC, y = speed, color ="9 Min"))+
+  scale_color_manual(name = "Sampling Interval", breaks = c("1 Min","3 Min", "6 Min","9 Min"), values = c("1 Min" = "cyan", "9 Min" = "magenta", "6 Min" = "blue", "3 Min" = "green"))
+# Higher Minutes has less difference.
 
 
 # Task 4
